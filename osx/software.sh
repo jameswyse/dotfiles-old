622dd025b75e59a8986a57ea3bba88942d8a452b
James Wyse <james@jameswyse.net>
2014-05-01 20:57:02 +1000

    Add storm to osx/software.sh

--- osx/software.sh
+++ osx/software.sh
@@ -7,5 +7,14 @@ then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
 fi
 
+#
 # Install packages from 'brews.txt'
+#
 cat $DOTFILES/osx/brews.txt | tr '\n' '\000' | xargs -0 brew install
+
+#
+# Install other tools
+#
+
+# Storm - SSH Manager https://github.com/emre/storm
+pip install storm
